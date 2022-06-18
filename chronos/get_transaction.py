d918f67bb0efb96ef0c008a65be09a029e36ee3b
unknown <roberts.mezals@gmail.com>
2022-06-18 12:19:02 +0300

    small push

--- chronos/get_transaction.py
+++ chronos/get_transaction.py
@@ -1,4 +1,6 @@
+import json
 
+from hexbytes import HexBytes
 from web3 import Web3
 
 # 2. Set web3 module
@@ -11,4 +13,29 @@ print(web3.isConnected())
 # 4. Get Block Number
 print(web3.eth.blockNumber)
 
-print(web3.eth.get_block(2000000))
+#print(web3.eth.get_block(14980535))
+
+class HexJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, HexBytes):
+            return obj.hex()
+        return super().default(obj)
+
+tx = web3.eth.get_block('pending')
+tx_dict = dict(tx)
+#print(tx_dict.transactions)
+
+
+for i in tx_dict ['transactions']:
+    #print(i.hex())
+    txs = web3.eth.get_transaction(i.hex())
+    txs_dict = dict(txs)
+    print(txs_dict.blockNumber)
+
+f.close()
+#tx_json = json.dumps(tx_dict, cls=HexJsonEncoder)
+
+
+#f = open("output.json", "w")
+#f.write(tx_json)
+#f.close()
